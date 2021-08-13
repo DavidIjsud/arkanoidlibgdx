2419ec70947e385c0cc3ec431fb1ab5ed214f390
David Garcia <david.garcia@oktana.com>
2021-08-13 12:47:35 -0400

    SAving for back

---
+++ core/src/com/badlogic/drop/src/clases/ControladorLadrilloBola.java
@@ -0,0 +1,14 @@
+package com.badlogic.drop.src.clases;
+
+
+public class ControladorLadrilloBola {
+
+         Ladrillo ladrillo;
+         Balon balon;
+
+        ControladorLadrilloBola(){
+            this.ladrillo = new Ladrillo();
+            this.balon = new Balon();
+        }
+
+}
